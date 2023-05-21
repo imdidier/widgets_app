f1b4cbafe3ff8daba9d9a7c5a6ef92ad6207921b
Didier Junco <imdidierjunco@gmail.com>
2023-05-21 15:15:58 -0500

    Menu Items list creado

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.icon,
+    required this.link,
+    required this.subTitle,
+    required this.title,
+  });
+}
+
+const List<MenuItem> appMenuItems = [
+  MenuItem(
+    icon: Icons.smart_button,
+    link: '/buttons',
+    subTitle: 'Los botones en Flutter ',
+    title: 'Botones',
+  ),
+  MenuItem(
+    icon: Icons.credit_card,
+    link: '/cards',
+    subTitle: 'Las tarjetas en Flutter',
+    title: 'Cards',
+  ),
+];
